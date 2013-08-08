e2b6fb131054cf0413976a65f4a713c6eb7ba8fc
Marcin Kolny [loganek] <marcin.kolny@gmail.com>
2013-08-09 00:58:19 +0200

    added splash screen

---
+++ src/src/com/bnt/bntshine/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.bnt.bntshine;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+	private static String TAG = SplashActivity.class.getName();
+	private static long SLEEP_TIME = 3; // Temporary there is a time. In the
+										// future we're going to use network
+										// data status
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN); 
+		
+		setContentView(R.layout.activity_splash);
+		IntentLauncher launcher = new IntentLauncher();
+		launcher.start();
+	}
+
+	private class IntentLauncher extends Thread {
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(SLEEP_TIME * 1000);
+			} catch (Exception e) {
+				Log.e(TAG, e.getMessage());
+			}
+
+			Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+			SplashActivity.this.startActivity(intent);
+			SplashActivity.this.finish();
+		}
+	}
+
+}
