81c0d7f0a1fe048099caf09691c44b2e0c034999
Marcin Kolny [loganek] <marcin.kolny@gmail.com>
2013-08-09 21:23:23 +0200

    implemented settings in a "network settings" module

--- src/src/com/bnt/bntshine/AppConfiguration.java
+++ src/src/com/bnt/bntshine/AppConfiguration.java
@@ -5,6 +5,9 @@ public final class AppConfiguration {
 	private static String MAIL_ADDRESS = "dwybranczyk@bntsystems.pl";
 	private static String MAIL_SUBJECT = "Pomoc BNT Shine";
 	private static String PHONE_NUMBER = "100000000";
+	private static String DEFAULT_IP = "192.168.0.123";
+	private static String DEFAULT_MASK = "255.255.255.0";
+	private static boolean DEFAULT_MODE = true; //true - manual, false - automatic
 	
 	public static String getWWW() {
 		return WWW;
@@ -20,5 +23,17 @@ public final class AppConfiguration {
 	
 	public static String getPhoneNumber() {
 		return PHONE_NUMBER;
+	}
+
+	public static String getDefaultIP() {
+		return DEFAULT_IP;
+	}
+	
+	public static String getDefaultMask() {
+		return DEFAULT_MASK;
+	}
+
+	public static boolean getDefaultMode() {
+		return DEFAULT_MODE;
 	}
 }
--- src/src/com/bnt/bntshine/NetworkSettingsActivity.java
+++ src/src/com/bnt/bntshine/NetworkSettingsActivity.java
@@ -1,15 +1,30 @@
 package com.bnt.bntshine;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class NetworkSettingsActivity extends Activity {
-
+	private EditText ipAddressET;
+	private EditText maskET;
+	private RadioGroup setSettingsRG;
+	private RadioButton manualSelect;
+	private RadioButton automaticSelect;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_network_settings);
+		
+		initPrivateFields();
+		loadConfig();
 	}
 
 	@Override
@@ -19,4 +34,46 @@ public class NetworkSettingsActivity extends Activity {
 		return true;
 	}
 
+	public void changeAddressClick(View view) {
+		saveSettings();
+	}
+	
+	private void initPrivateFields() {
+		ipAddressET = (EditText) findViewById(R.id.ipAddressEditText);
+		maskET = (EditText) findViewById(R.id.maskEditText);
+		setSettingsRG = (RadioGroup) findViewById(R.id.serverLocalizationRadioGroup);
+		manualSelect = (RadioButton) findViewById(R.id.manualLocalizeServerRadio);
+		automaticSelect = (RadioButton) findViewById(R.id.automaticLocalizeServerRadio);
+		
+		setSettingsRG.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) { 
+            	boolean enabling =  (checkedId == manualSelect.getId());
+            	ipAddressET.setEnabled(enabling);
+            	maskET.setEnabled(enabling);
+            }
+            });
+	}
+	
+	private void saveSettings() {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		SharedPreferences.Editor editor = preferences.edit();
+		
+		editor.putString("settings.network.ip", ipAddressET.getText().toString());
+		editor.putString("settings.network.mask", maskET.getText().toString());
+		editor.putBoolean("settings.network.select_mode", manualSelect.isChecked());
+
+		editor.commit();
+	}
+
+	private void loadConfig() {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+			
+		ipAddressET.setText(preferences.getString("settings.network.ip", AppConfiguration.getDefaultIP()));
+		maskET.setText(preferences.getString("settings.network.mask", AppConfiguration.getDefaultMask()));
+		
+		boolean selectionMode = preferences.getBoolean("settings.network.select_mode", AppConfiguration.getDefaultMode());
+		manualSelect.setChecked(selectionMode);
+		automaticSelect.setChecked(!selectionMode);
+	}
 }
