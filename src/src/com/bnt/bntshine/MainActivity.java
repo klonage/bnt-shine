3ed5bc8c264e6db3bd0a32a9dd63dbb1b5643d7a
Marcin Kolny [loganek] <marcin.kolny@gmail.com>
2013-08-09 19:09:37 +0200

    okienko dialogowe dla niezaimplementowanych funkcji

--- src/src/com/bnt/bntshine/MainActivity.java
+++ src/src/com/bnt/bntshine/MainActivity.java
@@ -1,23 +1,39 @@
 package com.bnt.bntshine;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	public void notImplementedFunctionClick(View view) {
+		AlertDialog alertDialog;
+		alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("Funkcja niedostępna");
+		alertDialog.setMessage("Obecnie funkcjonalność nie jest dostępna.");
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+
+		alertDialog.show();
+	}
+
 }
