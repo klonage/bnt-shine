7a734c1709b79c1072577c508321418cc92d3bff
Marcin Kolny [loganek] <marcin.kolny@gmail.com>
2013-08-10 15:12:43 +0200

    added globalitem to represent all available items in system

---
+++ src/src/com/bnt/bntshine/GlobalItem.java
@@ -0,0 +1,31 @@
+package com.bnt.bntshine;
+
+public class GlobalItem {
+	private String name;
+	private int group;
+	private int type; //i know what enum is, but I think int should be easier to use here.
+	
+	private static final int ICONS[] = {R.drawable.ic_launcher};
+	
+	public GlobalItem(String name, int group, int type) {
+		this.name = name;
+		this.group = group;
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getGroup() {
+		return group;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public int getIcon() {
+		return ICONS[type];
+	}
+}
